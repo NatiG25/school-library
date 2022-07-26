45bbc078ca0975fb1107c18457654824c16929cb
NatiG <nati.g5684@gmail.com>
2022-07-26 19:34:08 +0300

    Fix linter errors

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require_relative './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom, age, parent_perimission: true, name: 'Unknown')
     super(age, parent_perimission, name)
     @classroom = classroom
--- teacher.rb
+++ teacher.rb
@@ -1,7 +1,7 @@
 require_relative './person'
 
 class Teacher < Person
-    attr_accessor :specialization
+  attr_accessor :specialization
 
   def initialize(specialization, age, parent_perimission: true, name: 'Unknown')
     super(age, parent_perimission, name)
