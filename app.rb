0c9ba8e2463d366754c9b3667d99c6523800f038
NatiG <nati.g5684@gmail.com>
2022-07-31 00:41:14 +0300

    Remove args

--- app.rb
+++ app.rb
@@ -7,27 +7,27 @@ require './classroom'
 class App
   attr_reader :people, :books, :rentals
 
-  def initialize(people, books, rentals)
-    @people = people
-    @books = books
-    @rentals = rentals
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
   end
 
   def all_books
     @books.each do |book|
-      puts "Title: #{book.title}, Author: #{book.author}"
+      puts "[#{book.class}] - Title: #{book.title}, Author: #{book.author}"
     end
   end
 
   def all_people
     @people.each do |person|
-      puts "ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
+      puts "[#{person.class}] - ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
     end
   end
 
   def all_rentals
     @rentals.each do |rental|
-      puts "Name: #{rental.person.name}, Book: #{rental.book.title}, Date: #{rental.date}"
+      puts "[#{rental.class}] - Name: #{rental.person.name}, Book: #{rental.book.title}, Date: #{rental.date}"
     end
   end
 
