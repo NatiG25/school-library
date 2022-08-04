0a27e8878ffde68add503a291329def815772d5f
NatiG <nati.g5684@gmail.com>
2022-08-04 23:07:25 +0300

    Create book spec

---
+++ spec/book_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Book do
+
+    before :each do
+        @book = Book.new('Atomic', 'James')
+    end
+
+    describe '#new' do
+    it "Creates a new instance" do
+        @book.should be_an_instance_of Book
+        end
+    end
+end
