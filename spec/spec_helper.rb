835578ad1a6b8ca516454ac3db152221168e643e
NatiG <nati.g5684@gmail.com>
2022-08-04 23:07:08 +0300

    Create spec files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require_relative '../book'
+# require_relative '../capitalize'
+# require_relative '../classroom'
+# require_relative '../person'
+# require_relative '../rental'
+# require_relative '../student'
+# require_relative '../teacher'
+# require_relative '../trimmer'
+
